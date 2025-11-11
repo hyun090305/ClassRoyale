e3d2e9cb60567bca373425affd9459f528976542
ohtani1212 <sungwoo1212.park@gmail.com>
2025-11-11 15:03:41 +0900

    구조체넣음

--- main.c
+++ main.c
@@ -1,5 +1,41 @@
 #include <stdio.h>
 
+enum rank { 
+STUDENT = 0, TEACHER 
+}; 
+typedef struct {
+    char *name;
+    Bank bank;
+    char *id;
+    char *pw;
+    enum rank isadmin;
+    Item items[10];
+    int completed_missions;
+    int total_missions;
+    Mission mission[99];
+} User;
+
+typedef struct {
+    char *name;
+    int balance;
+    FILE *fp;
+    char rating;
+} Bank;
+
+typedef struct {
+    char *name;
+    int stock;
+    int cost;
+} Item;
+
+typedef struct {
+    char *name;
+    int type;
+    int reward;
+    int completed;
+} Mission;
+
+
 int main() {
     printf("Hello, WdddWWWWorld!");
 }
