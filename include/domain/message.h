f1ec766dfa65e15d00aa15f4c3b7a82d2c6fda1f
CU1121 <parksiyu1111@naver.com>
2025-11-25 00:19:45 +0900

    ..

---
+++ include/domain/message.h
@@ -0,0 +1,20 @@
+#ifndef DOMAIN_MESSAGE_H
+#define DOMAIN_MESSAGE_H
+
+#include "../types.h"
+
+#define MAX_MESSAGE_TEXT 256
+
+typedef struct PrivateMessage {
+    long timestamp;
+    char sender[50];
+    char recipient[50];
+    char body[MAX_MESSAGE_TEXT];
+} PrivateMessage;
+
+int message_send(const char *from, const char *to, const char *body);
+int message_recent_to_buf(const char *username, int limit, char *buf, size_t buflen);
+int message_thread_to_buf(const char *username, const char *peer, int limit, char *buf, size_t buflen);
+int message_list_partners(const char *username, char partners[][50], int max_partners);
+
+#endif /* DOMAIN_MESSAGE_H */
